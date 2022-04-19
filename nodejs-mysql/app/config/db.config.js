1a36450fb6a94fa998f1867a2fad28f6e57f6cb3
Lolomyn <61377364+Lolomyn@users.noreply.github.com>
2022-04-19 07:20:06 +0300

    Update db.config.js

--- nodejs-mysql/app/config/db.config.js
+++ nodejs-mysql/app/config/db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "12345678",
-    DB: "basa_dp",
-    dialect: "mysql",
-    // filePath: "C:\\Users\\bons1\\Documents\\git\\MirITeam\\project\\mirITeam\\src\\files",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "12345678",
+  DB: "basa_dp",
+  dialect: "mysql",
+  // filePath: "C:\\Users\\bons1\\Documents\\git\\MirITeam\\project\\mirITeam\\src\\files",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
